5394d9968b81a75300f806a8388c7f6be31ff5d7
Gh0stDan <justlivedan@live.com>
2021-03-08 17:17:34 -0600

    First

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController2D Controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;   //a = -1    d = 1
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump); //move, crouch, jump   
+        jump = false;
+    }
+}
